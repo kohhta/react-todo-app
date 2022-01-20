ccfbaa454356817f2b8912b27fc2270a38ae53f3
kota_koyama <kouta103177@gmail.com>
2022-01-20 22:07:18 +0900

    initial commit

---
+++ todo-web/src/App.js
@@ -0,0 +1,15 @@
+import Task from './component/Task';
+
+const App = () => {
+  return (
+    <>
+      <h1>タスク一覧</h1>
+      <Task name="買い物" />
+      <Task name="ランニング" />
+      <Task name="プログラミングの勉強" />
+      <Task name="ささささｓ"/>
+    </>
+  );
+}
+
+export default App;
