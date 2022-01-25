52d5e31b526a67ca66379a4832852e1b7743d7af
kota_koyama <kouta103177@gmail.com>
2022-01-26 00:57:33 +0900

    ChakraUIを使ってTaskコンポーネントのデザインを整える

--- todo-web/src/App.js
+++ todo-web/src/App.js
@@ -1,14 +1,32 @@
-import Task from './component/Task';
+import Task from "./component/Task";
+import { Center, Box, CheckboxGroup, Text } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <>
-      <h1>タスク一覧</h1>
-      <Task name="買い物" />
-      <Task name="ランニング" />
-      <Task name="プログラミングの勉強" />
-      <Task name="ささささｓ"/>
-    </>
+    // <>
+    //   <h1>タスク一覧</h1>
+    //   <Task name="買い物" />
+    //   <Task name="ランニング" />
+    //   <Task name="プログラミングの勉強" />
+    //   <Task name="ささささｓ"/>
+    // </>
+    <Box mt="64px">
+      <Center>
+        <Box>
+        <Box mb="24px">
+            <Text fontSize="24px" fontWeight="bold">
+              タスク一覧
+            </Text>
+          </Box>
+          <CheckboxGroup>
+            <Task name="買い物" />
+            <Task name="ランニング" />
+            <Task name="プログラミングの勉強" />
+          </CheckboxGroup>
+        </Box>
+      </Center>
+    </Box>
+
   );
 }
 
